2839fe2207cf0e1217e12d1d9dc968dee1ba1794
gentksb <gen@gensobunya.net>
2022-06-15 13:34:48 +0900

    OK

--- install.sh
+++ install.sh
@@ -1,34 +1,32 @@
 #!/usr/bin/env bash
-set -ue
+set -xue
 sudo apt-get update
 sudo apt-get install -y direnv git
 
 link_to_homedir() {
   command echo "backup old dotfiles..."
+
   if [ ! -d "$HOME/.dotbackup" ];then
     command echo "$HOME/.dotbackup not found. Auto Make it"
     command mkdir "$HOME/.dotbackup"
   fi
 
-  local script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
-  local dotdir=$(dirname ${script_dir})
-  if [[ "$HOME" != "$dotdir" ]];then
-    for f in $dotdir/.??*; do
-      [[ `basename $f` == ".git" ]] && continue
-      [[ `basename $f` == ".github" ]] && continue
-      [[ `basename $f` == ".devcontainer" ]] && continue
-      if [[ -L "$HOME/`basename $f`" ]];then
-        command rm -f "$HOME/`basename $f`"
-      fi
-      if [[ -e "$HOME/`basename $f`" ]];then
-        command mv "$HOME/`basename $f`" "$HOME/.dotbackup"
-      fi
-      command ln -snf $f $HOME
-      echo $f
-    done
-  else
-    command echo "same install src dest"
-  fi
+  local script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd )"
+
+  for dotfile in $script_dir/.??* ; do
+    [[ `basename $dotfile` == ".git" ]] && continue
+    [[ `basename $dotfile` == ".github" ]] && continue
+    [[ `basename $dotfile` == ".devcontainer" ]] && continue
+    if [[ -L "$HOME/`basename $dotfile`" ]];then
+      command rm -f "$HOME/`basename $dotfile`"
+    fi
+    if [[ -e "$HOME/`basename $dotfile`" ]];then
+      command mv "$HOME/`basename $dotfile`" "$HOME/.dotbackup"
+    fi
+    command ln -snf $dotfile $HOME
+    echo $dotfile
+  done
 }
 
 link_to_homedir
+command echo -e "\e[1;36m Install completed!!!! \e[m"
